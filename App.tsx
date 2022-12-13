a52651afbf75691261eb005c9ffd08fb79ff05e1
jun <z3033162@edu.gifu-u.ac.jp>
2022-12-13 15:41:00 +0900

    TODOts

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <>
+      <h2>- Todo List -</h2>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//https://terupro.net/react-ts-todo-sample/
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
